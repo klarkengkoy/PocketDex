34eb34058688b730684143d484cffa8fbc7d45c2
klarkengkoy <klarkengkoy@users.noreply.github.com>
2026-09-07 23:17:28 +0900

    chore: remove Options/Hub placeholder screens and AppPreferences
    Delete the hardcoded Items/Moves/Strategy placeholder screens
    (HubScreens.kt), the Options screen/ViewModel, and the now-unused
    AppPreferences DataStore wrapper, per the nav rework that dropped
    these destinations in favor of Moves/Types/Items/Team.

--- app/src/main/java/com/samidevstudio/pocketdex/PocketDexApplication.kt
+++ app/src/main/java/com/samidevstudio/pocketdex/PocketDexApplication.kt
@@ -2,16 +2,13 @@ package com.samidevstudio.pocketdex
 
 import android.app.Application
 import com.samidevstudio.pocketdex.data.AppContainer
-import com.samidevstudio.pocketdex.data.AppPreferences
 import com.samidevstudio.pocketdex.data.DefaultAppContainer
 
 class PocketDexApplication : Application() {
     lateinit var container: AppContainer
-    lateinit var appPreferences: AppPreferences
 
     override fun onCreate() {
         super.onCreate()
-        appPreferences = AppPreferences(this)
         container = DefaultAppContainer(this)
     }
 }
